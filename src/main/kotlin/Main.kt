dbb1a6f5f58b08a76618984f286f80e0e9ff3cec
sureshbabua8 <amirthasureshbabu@gmail.com>
2020-06-08 11:36:59 -0500

    switch to org.json library, clean up GET request code

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -2,10 +2,10 @@ package hello
 
 import com.fasterxml.jackson.core.util.DefaultIndenter
 import com.fasterxml.jackson.core.util.DefaultPrettyPrinter
-import com.fasterxml.jackson.databind.ObjectMapper
+//import com.fasterxml.jackson.databind.ObjectMapper
 import com.fasterxml.jackson.databind.SerializationFeature
-import com.fasterxml.jackson.dataformat.xml.XmlMapper
-import com.fasterxml.jackson.module.kotlin.registerKotlinModule
+//import com.fasterxml.jackson.dataformat.xml.XmlMapper
+//import com.fasterxml.jackson.module.kotlin.registerKotlinModule
 import io.ktor.application.Application
 import io.ktor.application.call
 import io.ktor.application.install
@@ -14,6 +14,7 @@ import io.ktor.client.engine.cio.CIO
 import io.ktor.client.request.get
 import io.ktor.features.ContentNegotiation
 import io.ktor.jackson.jackson
+import io.ktor.response.respond
 import io.ktor.response.respondText
 import io.ktor.routing.get
 import io.ktor.routing.routing
@@ -25,8 +26,6 @@ import org.json.XML
 val client = HttpClient(CIO)
 fun Application.viewCourse() {
     var xml = ""
-    var xmlMapper: ObjectMapper = XmlMapper().registerKotlinModule()
-    val objectMapper = ObjectMapper()
 
 
     install(ContentNegotiation) {
@@ -47,10 +46,8 @@ fun Application.viewCourse() {
             try {
                 xml = client.get<String>("https://courses.illinois.edu/cisapp/explorer/schedule/" +
                         call.parameters["year"] + ".xml")
-//                    .replace(".xml\">", ".xml\"><label>")
-//                    .replace("</term>", "</label></term>")
-                val value: Year = xmlMapper.readValue(xml, Year::class.java)
-                call.respondText(objectMapper.writerWithDefaultPrettyPrinter().writeValueAsString(value))
+                val xmlJSONObj: JSONObject = XML.toJSONObject(xml)
+                call.respondText(xmlJSONObj.toString(4))
             } catch (e: Exception) {
                 call.respondText("Invalid Request!  Input a valid year.")
             }
@@ -59,65 +56,35 @@ fun Application.viewCourse() {
             try {
                 xml = client.get<String>("https://courses.illinois.edu/cisapp/explorer/schedule/" +
                         call.parameters["year"] + "/" + call.parameters["term"] + ".xml")
-                    .replace(".xml\">", ".xml\"><label>")
-                    .replace("</subject>", "</label></subject>")
-                    .replace("</calendarYear>", "</label></calendarYear>")
-                val value: Term = xmlMapper.readValue(xml, Term::class.java)
-                call.respondText(objectMapper.writerWithDefaultPrettyPrinter().writeValueAsString(value))
+                val xmlJSONObj: JSONObject = XML.toJSONObject(xml)
+                call.respondText(xmlJSONObj.toString(4))
+
             } catch (e: Exception) {
                 call.respondText("Invalid Request!  Input a valid year and term.")
             }
         }
+
         get("/{year}/{term}/{course}") {
             xml = client.get<String>("https://courses.illinois.edu/cisapp/explorer/schedule/" +
                     call.parameters["year"] + "/" + call.parameters["term"] + "/" + call.parameters["course"] + ".xml")
-                .replace(".xml\">", ".xml\"><label>")
-                .replace("</subject>", "</label></subject>")
-                .replace("</calendarYear>", "</label></calendarYear>")
-                .replace("</term>", "</label></term>")
-                .replace("</course>", "</label></course>")
-            val value: Subject = xmlMapper.readValue(xml, Subject::class.java)
-            call.respondText(objectMapper.writerWithDefaultPrettyPrinter().writeValueAsString(value))
+            val xmlJSONObj: JSONObject = XML.toJSONObject(xml)
+            call.respondText(xmlJSONObj.toString(4))
 
         }
+
         get("/{year}/{term}/{course}/{code}") {
             xml = client.get<String>("https://courses.illinois.edu/cisapp/explorer/schedule/" +
                     call.parameters["year"] + "/" + call.parameters["term"] + "/" + call.parameters["course"] + "/" +
                     call.parameters["code"] + ".xml")
-                .replace(".xml\">", ".xml\"><label>")
-                .replace("</subject>", "</label></subject>")
-                .replace("</calendarYear>", "</label></calendarYear>")
-                .replace("</term>", "</label></term>")
-                .replace("</section>", "</label></section>")
-                .replace("</genEdAttribute", "</label></genEdAttribute>")
-            val value: Subject = xmlMapper.readValue(xml, Subject::class.java)
-            call.respondText(objectMapper.writerWithDefaultPrettyPrinter().writeValueAsString(value))
+            val xmlJSONObj: JSONObject = XML.toJSONObject(xml)
+            println(xmlJSONObj)
+            call.respondText(xmlJSONObj.toString(4))
+
+
 
         }
     }
 }
- suspend fun main() {
-     var xml = ""
-     var xmlMapper: ObjectMapper = XmlMapper().registerKotlinModule()
-     val objectMapper = ObjectMapper()
-     xml = client.get<String>("https://courses.illinois.edu/cisapp/explorer/schedule/2020.xml")
-//                    .replace(".xml\">", ".xml\"><label>")
-//                    .replace("</term>", "</label></term>")
-     println(xml)
-//     val value: Year = xmlMapper.readValue(xml, Year::class.java)
-     xml = client.get<String>("https://courses.illinois.edu/cisapp/explorer/schedule/2020/fall/CS/125.xml")
-     val xmlJSONObj: JSONObject = XML.toJSONObject(xml)
-     val jsonPrettyPrintString: String = xmlJSONObj.toString(4)
-     println(jsonPrettyPrintString)
-//         .replace(".xml\">", ".xml\"><label>")
-//         .replace("</subject>", "</label></subject>")
-//         .replace("</calendarYear>", "</label></calendarYear>")
-//         .replace("</term>", "</label></term>")
-//         .replace("</section>", "</label></section>")
-////         .replace("</genEdAttribute>", "</label></genEdAttribute>")
-//         .replaceFirst(".xml\"><label>", ".xml\">")
-//     val value: Class = xmlMapper.readValue(xml, Class::class.java)
-//     println(objectMapper.writerWithDefaultPrettyPrinter().writeValueAsString(value))
+ fun main() {
      embeddedServer(Netty, port = 8000, module = Application::viewCourse).start(wait = true)
-
  }
