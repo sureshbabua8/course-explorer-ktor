ed4dc6b77a03448ce58747b77cbe3dfc9c32a2b7
sureshbabua8 <amirthasureshbabu@gmail.com>
2020-06-21 22:16:00 -0500

    add time updates to cache

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -19,9 +19,33 @@ import io.ktor.server.engine.embeddedServer
 import io.ktor.server.netty.Netty
 import java.io.File
 
+private val client = HttpClient(CIO)
+private val resDir: String = "src/main/cache/schedule"
+
+private suspend fun updateCache(originalUri: String, timeLapse: Long): String {
+    val newUri = if (originalUri == "/") {
+        originalUri.trim('/')
+    } else {
+        originalUri
+    }
+    val path: String = resDir + originalUri.replace('/', '-')
+    val xmlString: String
+    val file: File = File("$path.xml")
+    if (file.exists() && file.lastModified() < timeLapse) {
+        xmlString = file.readText()
+    } else {
+        xmlString = client.get("https://courses.illinois.edu/cisapp/explorer/schedule$newUri.xml")
+        File("$path.xml").writeText(xmlString)
+    }
+
+    return xmlString
+}
+
 fun Application.run() {
-    val client = HttpClient(CIO)
-    val resDir: String = "src/main/resources/schedule"
+    val year: Long = 31540000000
+    val month: Long = 2628000000
+    val day: Long = 86400000
+    val hour: Long = 3600000
 
     install(ContentNegotiation) {
         jackson {
@@ -35,92 +59,28 @@ fun Application.run() {
 
     routing {
         get("/") {
-            val uri: String = call.request.uri.trim('/')
-            val path: String = resDir + call.request.uri.replace('/', '-')
-            val xmlString: String
-            val file: File = File("$path.xml")
-            if (file.exists()) {
-                xmlString = file.readText()
-            } else {
-                xmlString = client.get("https://courses.illinois.edu/cisapp/explorer/schedule$uri.xml")
-                File("$path.xml").writeText(xmlString)
-            }
-
-            call.respond(xmlString.fromXml<CalendarYears>())
+            call.respond(updateCache(call.request.uri, year).fromXml<CalendarYears>())
         }
         get("/{year}") {
-            val uri: String = call.request.uri
-            val path: String = resDir + call.request.uri.replace('/', '-')
-            val xmlString: String
-            val file: File = File("$path.xml")
-            if (file.exists()) {
-                xmlString = file.readText()
-            } else {
-                xmlString = client.get("https://courses.illinois.edu/cisapp/explorer/schedule$uri.xml")
-                File("$path.xml").writeText(xmlString)
-            }
-
-            call.respond(xmlString.fromXml<ScheduleYear>())
+            call.respond(updateCache(call.request.uri, month).fromXml<ScheduleYear>())
 
         }
         get("/{year}/{term}/") {
-            val uri: String = call.request.uri
-            val path: String = resDir + call.request.uri.replace('/', '-')
-            val xmlString: String
-            val file: File = File("$path.xml")
-            if (file.exists()) {
-                xmlString = file.readText()
-            } else {
-                xmlString = client.get("https://courses.illinois.edu/cisapp/explorer/schedule$uri.xml")
-                File("$path.xml").writeText(xmlString)
-            }
-
-            call.respond(xmlString.fromXml<Term>())
+            call.respond(updateCache(call.request.uri, day).fromXml<Term>())
         }
 
         get("/{year}/{term}/{course}") {
-            val uri: String = call.request.uri
-            val path: String = resDir + call.request.uri.replace('/', '-')
-            val xmlString: String
-            val file: File = File("$path.xml")
-            if (file.exists()) {
-                xmlString = file.readText()
-            } else {
-                xmlString = client.get("https://courses.illinois.edu/cisapp/explorer/schedule$uri.xml")
-                File("$path.xml").writeText(xmlString)
-            }
-
-            call.respond(xmlString.fromXml<Department>())
+            call.respond(updateCache(call.request.uri, hour).fromXml<Department>())
         }
 
         get("/{year}/{term}/{course}/{code}") {
-            val uri: String = call.request.uri
-            val path: String = resDir + call.request.uri.replace('/', '-')
-            val xmlString: String
-            val file: File = File("$path.xml")
-            if (file.exists()) {
-                xmlString = file.readText()
-            } else {
-                xmlString = client.get("https://courses.illinois.edu/cisapp/explorer/schedule$uri.xml")
-                File("$path.xml").writeText(xmlString)
-            }
-
-            call.respond(xmlString.fromXml<SubjectCourse>())
+            call.respond(updateCache(call.request.uri, hour).fromXml<SubjectCourse>())
         }
         get("/{year}/{term}/{course}/{code}/{section}") {
-            val uri: String = call.request.uri
-            val path: String = resDir + call.request.uri.replace('/', '-')
-            val xmlString: String
-            val file: File = File("$path.xml")
-            if (file.exists()) {
-                xmlString = file.readText()
-            } else {
-                xmlString = client.get("https://courses.illinois.edu/cisapp/explorer/schedule$uri.xml")
-                File("$path.xml").writeText(xmlString)
-            }
-
-            call.respond(xmlString.fromXml<Section>())
+            call.respond(updateCache(call.request.uri, hour).fromXml<Section>())
         }
+
+
     }
 }
 
